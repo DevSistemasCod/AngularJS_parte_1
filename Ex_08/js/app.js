002c914238bb49581f84cc5ead29778f8b5a588a
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-12-09 20:51:26 -0300

    Delete Ex_08 directory

--- Ex_08/js/app.js
+++
@@ -1,9 +0,0 @@
-let app = angular.module('minhaAplicacao', []);
-
-app.controller('meuController', function($scope) {
-    $scope.carros = {
-        carro01: { marca: "Ford", modelo: "Ká", cor: "branco" },
-        carro02: { marca: "Fiat", modelo: "Uno", cor: "azul" },
-        carro03: { marca: "Toyota", modelo: "Corolla", cor: "verde" }
-    };
-});
